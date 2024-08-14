62830a375eaf64450d5e624da42cdf56d7b04bcc
Kurt <breath103@gmail.com>
2024-08-14 14:40:27 +0900

    update swift toolings
    fix build for spm

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
   name: "Swime",
   platforms: [
-    .iOS(.v9),
-    .macOS(.v10_10)
+    .iOS(.v12),
+    .macOS(.v11)
   ],
   products: [
     .library(name: "Swime", targets: ["Swime"])
